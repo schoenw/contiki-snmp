b34055fde192ae38a23fa1ae167245684a971ef6
Anuj Sehgal <s.anuj@jacobs-university.de>
2012-12-18 14:54:46 +0100

    Added back SNMP example.

--- examples/minimal-net.sh
+++
@@ -1,5 +0,0 @@
-sudo route add -6 aaaa::/64 tap0
-sudo ip -6 address add aaaa::1/64 dev tap0
-sudo sysctl -w net.ipv6.conf.all.forwarding=1
-sudo /etc/init.d/radvd start
-sudo /etc/init.d/radvd restart
--- examples/routing.sh
+++
@@ -1,4 +0,0 @@
-sudo ip -6 address add aaaa::1/64 dev usb0
-sudo sysctl -w net.ipv6.conf.all.forwarding=1
-sudo /etc/init.d/radvd start
-sudo /etc/init.d/radvd restart
